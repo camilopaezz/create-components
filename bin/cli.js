6bc761aa52d71fdee7a5d97c37d71b73a6615c90
Camilo Paez <camilopaez.dev@gmail.com>
2021-07-07 13:27:10 -0500

    feat(cli)!: basic usage completed

--- bin/cli.js
+++ bin/cli.js
@@ -1,31 +1,40 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer')
+const fs = require('fs/promises')
+const path = require('path')
 
-const hasAtomic = (answers) => {
-  return answers.useAtomic
-}
+const addons = require('../lib/addons')
+const questions = require('../lib/questions')
+
+const execDir = process.cwd()
 
 ;(async () => {
-  const answers = await inquirer.prompt([
-    {
-      name: 'name',
-      type: 'input',
-      message: "What's the name of your component?"
-    },
-    {
-      name: 'useAtomic',
-      message: 'Do you use Atomic Design?',
-      type: 'confirm'
-    },
-    {
-      name: 'type',
-      message: 'What kind is your component?',
-      type: 'list',
-      when: hasAtomic,
-      choices: ['atom', 'molecule', 'organism', 'template', 'page']
+  const answers = await inquirer.prompt(questions)
+
+  try {
+    const targetPath = answers.type
+      ? path.join(execDir, 'components', `${answers.type}s`, answers.name)
+      : path.join(execDir, 'components', `${answers.type}s`, answers.name)
+
+    await fs.mkdir(targetPath, {
+      recursive: true
+    })
+
+    await fs.writeFile(path.join(targetPath, 'index.js'), addons.Component(answers).template, {
+      encoding: 'utf-8'
+    })
+
+    for (const addon of answers.addons) {
+      const selectedAddon = addons[addon](answers)
+      const selectedAddonFilename = path.join(targetPath, selectedAddon.filename)
+      await fs.writeFile(selectedAddonFilename, selectedAddon.template, {
+        encoding: 'utf-8'
+      })
     }
-  ])
+  } catch (error) {
+    console.error(error)
+  }
 
   console.log(`🔧 Your component: "${answers.name}" was successfully created`)
 })()
